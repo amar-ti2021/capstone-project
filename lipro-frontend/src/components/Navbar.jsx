7a999d440605b4afcc825dd45d5ee31aa30c1b84
merlianats <merlianatris@gmail.com>
2024-06-06 22:58:29 +0700

    Menambahkan react router

--- lipro-frontend/src/components/Navbar.jsx
+++ lipro-frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Logo from "../assets/logo-lipro.png";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -18,7 +19,7 @@ const Navbar = () => {
                         </svg>
                     </button>
                     <div className="hidden w-full absolute right-10 md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                             <li>
                             <a href="#" className="block py-2 px-3 font-inter text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-white" aria-current="page">Get Started</a>
                             </li>
@@ -29,7 +30,7 @@ const Navbar = () => {
                             <a href="#our-team" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Team</a>
                             </li>
                             <li>
-                            <a href="#" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log In</a>
+                            <Link to="/login" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log In</Link>
                             </li>
                         </ul>
                     </div>
--- lipro-frontend/src/main.jsx
+++ lipro-frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import LandingPages from './pages/LandingPages.jsx'
+import Login from './pages/LogIn.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPages />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
