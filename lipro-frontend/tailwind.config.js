e199cb4c2219ac10de8aafe5b43c8dbd4350c220
merlianats <merlianatris@gmail.com>
2024-06-08 15:24:41 +0700

    Menambahkan footer

--- lipro-frontend/tailwind.config.js
+++ lipro-frontend/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         'bright-blue': '#40A2E3',
         'pale-blue': '#DEF1FF',
         'moderate-blue': '#40679E',
-        'soft-blue': '#4AA7E5'
+        'soft-blue': '#4AA7E5',
+        'pure-blue': '#008DDA'
 
       },
       fontFamily: {
