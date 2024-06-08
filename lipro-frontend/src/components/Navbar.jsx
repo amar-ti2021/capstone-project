6ac97f0a23d5adf7d22f2475e3d1911894c69226
Ahmad Badar <ahmadbadar145@gmail.com>
2024-06-09 00:54:43 +0700

    Memperbaiki desain navbar

--- lipro-frontend/src/components/Navbar.jsx
+++ lipro-frontend/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <div className="hidden w-full absolute right-10 md:block md:w-auto" id="navbar-default">
                         <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                             <li>
-                            <a href="#" className="block py-2 px-3 font-inter text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-white" aria-current="page">Get Started</a>
+                            <a href="#getStarted" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Get Started</a>
                             </li>
                             <li>
                             <a href="#product" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Product</a>
