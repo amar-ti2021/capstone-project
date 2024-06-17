928dc4cc3af5790f6289faafb73a101c73d09730
Ahmad Badar <ahmadbadar145@gmail.com>
2024-06-17 23:40:27 +0700

    Memperbaiki navbar halaman statistic dan menambahkan hamburger button

--- lipro-frontend/src/pages/Statistic.jsx
+++ lipro-frontend/src/pages/Statistic.jsx
@@ -59,6 +59,12 @@ const options = {
 }
 
 const Statistic = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [chart, setChart] = useState(null);
 
@@ -89,42 +95,32 @@ const Statistic = () => {
             <nav className="bg-bright-blue p-4">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src={Logo} className="h-16" alt="Lipro" />
-                        <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap dark:text-white">LiPro</span>
+                    <img src={Logo} className="h-16" alt="Lipro" />
+                    <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap dark:text-white">LiPro</span>
                     </a>
-                    <div className="hidden w-full absolute left-52 md:block md:w-auto">
-                        <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                            <li>
-                                <a href="/dashboard" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="/statistic" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</a>
-                            </li>
-                        </ul>
+                    <div className="md:hidden">
+                    <button onClick={toggleMenu} className="block text-white focus:outline-none">
+                        <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z"
+                        />
+                        </svg>
+                    </button>
                     </div>
-                    <div className="relative">
-                        <button 
-                            id="dropdownDividerButton" 
-                            className="text-white text-lg font-inter focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
-                            type="button"
-                            onClick={toggleDropdown}
-                        >
-                            Hello, Badar 
-                            <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                            </svg>
-                        </button>
-
-                        <div 
-                            id="dropdownDivider" 
-                            className={`absolute right-0 z-10 ${isDropdownOpen ? 'block' : 'hidden'} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
-                        >
-                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDividerButton">
-                                <li>
-                                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logout</a>
-                                </li>
-                            </ul>
-                        </div>
+                    <div className={`w-full md:w-auto md:flex ${menuOpen ? 'block' : 'hidden'}`}>
+                    <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                        <li>
+                        <a href="/dashboard" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Dashboard</a>
+                        </li>
+                        <li>
+                        <a href="/statistic" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</a>
+                        </li>
+                        <li>
+                        <a href="/" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Sign Out</a>
+                        </li>
+                    </ul>
                     </div>
                 </div>
             </nav>
