6746fd133a9d1f00b4ca3767eb59f2a82edac194
Ahmad Badar <ahmadbadar145@gmail.com>
2024-06-18 00:07:16 +0700

    Perbaikan drawer

--- lipro-frontend/src/components/Navbar.jsx
+++ lipro-frontend/src/components/Navbar.jsx
@@ -3,52 +3,48 @@ import Logo from "../assets/logo-lipro.png";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleMenuToggle = () => {
-        setIsMenuOpen(!isMenuOpen);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
     };
 
     return (
-        <div>
-            <nav className="bg-bright-blue relative">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <nav className="bg-bright-blue p-4">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src={Logo} className="h-16" alt="Lipro" />
-                        <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap dark:text-white">LiPro</span>
+                    <img src={Logo} className="h-16" alt="Lipro" />
+                    <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap dark:text-white">LiPro</span>
                     </a>
-                    <button 
-                        data-collapse-toggle="navbar-default" 
-                        type="button" 
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
-                        aria-controls="navbar-default" 
-                        aria-expanded={isMenuOpen} 
-                        onClick={handleMenuToggle}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    <div className="md:hidden">
+                    <button onClick={toggleMenu} className="block text-white focus:outline-none">
+                        <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z"
+                        />
                         </svg>
                     </button>
-                    <div className={`absolute top-20 right-0 left-0 transition-all duration-300 ease-in-out transform ${isMenuOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-95'} bg-bright-blue md:block md:w-auto z-50`} id="navbar-default">
-                        <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                            <li>
-                                <a href="#getStarted" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Get Started</a>
-                            </li>
-                            <li>
-                                <a href="#product" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Product</a>
-                            </li>
-                            <li>
-                                <a href="#our-team" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Team</a>
-                            </li>
-                            <li>
-                                <Link to="/login" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log In</Link>
-                            </li>
-                        </ul>
+                    </div>
+                    <div className={`w-full md:w-auto md:flex ${menuOpen ? 'block' : 'hidden'}`}>
+                    <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                        <li>
+                        <a href="/" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Get Started</a>
+                        </li>
+                        <li>
+                        <a href="#product" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Product</a>
+                        </li>
+                        <li>
+                        <a href="#our-team" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Our Team</a>
+                        </li>
+                        <li>
+                        <a href="/login" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Sign In</a>
+                        </li>
+                    </ul>
                     </div>
                 </div>
             </nav>
-        </div>
     );
 };
 
