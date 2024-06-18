d41bad9dec109b35bf683aa74d5b4f33517c5ecd
Ahmad Badar <ahmadbadar145@gmail.com>
2024-06-18 18:40:41 +0700

    Menggunakan routing yang benar dan mengatur layout dasar aplikasi

--- lipro-frontend/src/App.jsx
+++ lipro-frontend/src/App.jsx
@@ -1,14 +1,22 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import LandingPages from './pages/LandingPages'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Dashboard from './pages/Dashboard';
+import LandingPages from './pages/LandingPages';
+import Login from './pages/LogIn';
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <LandingPages />
-    </>
-  )
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPages />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
