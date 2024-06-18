fff3daf76f957d06bbbc2c19d550b0fb14ecf568
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2024-06-18 22:59:15 +0700

    feat: add several new Endpoints

--- lipro-backend/routes/api.js
+++ lipro-backend/routes/api.js
@@ -1,21 +1,27 @@
-import express from "express";
-import AuthController from "../controllers/AuthController.js";
-import TodoController from "../controllers/TodoController.js";
+import express from 'express'
+import AuthController from '../controllers/AuthController.js'
+import TaskController from '../controllers/TaskController.js'
+import StatisticController from '../controllers/StatisticController.js'
 
-const router = express.Router();
-router.post("/login", AuthController.login);
-router.post("/register", AuthController.register);
+const router = express.Router()
+router.post('/login', AuthController.login)
+router.post('/register', AuthController.register)
 
-router.post("/protected/check-auth", AuthController.checkLogin);
+router.post('/protected/check-auth', AuthController.checkLogin)
 
-router.get("/protected/todo", TodoController.index);
-router.get("/protected/todo/:id", TodoController.show);
-router.put("/protected/todo/:id", TodoController.update);
-router.delete("/protected/todo/:id", TodoController.destroy);
-router.post("/protected/todo", TodoController.store);
+router.get('/protected/tasks', TaskController.index)
+router.get('/protected/tasks/completed', TaskController.completed)
+router.get('/protected/tasks/uncompleted', TaskController.uncompleted)
+router.get('/protected/tasks/today', TaskController.today)
+router.get('/protected/tasks/weekly', TaskController.weekly)
+router.get('/protected/tasks/:id', TaskController.show)
+router.get('/protected/statistics', StatisticController.index)
+router.put('/protected/tasks/:id', TaskController.update)
+router.delete('/protected/tasks/:id', TaskController.destroy)
+router.post('/protected/tasks', TaskController.store)
 
-router.all("*", (req, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
+router.all('*', (req, res) => {
+  res.status(404).json({ error: 'Not Found' })
+})
 
-export default router;
+export default router
