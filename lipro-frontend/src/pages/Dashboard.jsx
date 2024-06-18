42234128d4d6ab1f38e3603a78edc1a26f352116
Ahmad Badar <ahmadbadar145@gmail.com>
2024-06-18 18:41:07 +0700

    Menambahkan pengecekan autentikasi

--- lipro-frontend/src/pages/Dashboard.jsx
+++ lipro-frontend/src/pages/Dashboard.jsx
@@ -1,129 +1,86 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import Logo from "../assets/logo-lipro.png";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [taskName, setTaskName] = useState('');
     const [startTime, setStartTime] = useState('');
     const [endTime, setEndTime] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [date, setDate] = useState(new Date().toLocaleDateString());
+    const [day, setDay] = useState(new Date().getDay());
+    const navigate = useNavigate();
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const handleAddTask = () => {
-        console.log("Task Name:", taskName);
-        console.log("Start Time:", startTime);
-        console.log("End Time:", endTime);
-
+        setTasks([...tasks, { name: taskName, start: startTime, end: endTime }]);
         setTaskName('');
         setStartTime('');
         setEndTime('');
         closeModal();
     };
 
-    // time
-    const cTime = new Date().toLocaleTimeString();
-    const [time, setTime] = useState(cTime);
-
-    const updateTime = () => {
-        const newTime = new Date().toLocaleTimeString();
-        setTime(newTime);
-    }
-
     useEffect(() => {
-        const timeInterval = setInterval(updateTime, 1000);
+        const timeInterval = setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
+        const dateInterval = setInterval(() => setDate(new Date().toLocaleDateString()), 1000);
+        const dayInterval = setInterval(() => setDay(new Date().getDay()), 1000);
         return () => {
             clearInterval(timeInterval);
-        }
-    }, [time]);
-    
-    // date
-    const cDate = new Date().toLocaleDateString()
-    const [date, setDate] = useState(cDate)
-    
-    const updateDate = () =>{
-        const newDate = new Date().toLocaleDateString()
-        setDate(newDate)
-    }
-    
-    useEffect(() =>{
-        const dateInterval = setInterval(updateDate, 1000)
-        return () =>{
-            clearInterval(dateInterval)
-        }
-    
-    }, [date])
-    
-    // day
-    const cDay = new Date().getDay()
-    const [day, setDay] = useState(cDay)
-    const weekDays = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    
-    const updateDay = () =>{
-        const newDay = new Date().getDay()
-        setDay(newDay)
-    }
-    
-    useEffect(() =>{
-        const dayInterval = setInterval(updateTime, 1000)
-        return () =>{
-            clearInterval(dayInterval)
-        }
-    
-    }, [day])
+            clearInterval(dateInterval);
+            clearInterval(dayInterval);
+        };
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
 
     return (
         <div>
             <nav className="bg-bright-blue p-4">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src={Logo} className="h-16" alt="Lipro" />
-                    <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap dark:text-white">LiPro</span>
-                    </a>
+                    <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src={Logo} className="h-16" alt="Lipro" />
+                        <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap">LiPro</span>
+                    </Link>
                     <div className="md:hidden">
-                    <button onClick={toggleMenu} className="block text-white focus:outline-none">
-                        <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z"
-                        />
-                        </svg>
-                    </button>
+                        <button onClick={toggleMenu} className="block text-white focus:outline-none">
+                            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z" />
+                            </svg>
+                        </button>
                     </div>
                     <div className={`w-full md:w-auto md:flex ${menuOpen ? 'block' : 'hidden'}`}>
-                    <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                        <li>
-                        <a href="/dashboard" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Dashboard</a>
-                        </li>
-                        <li>
-                        <a href="/statistic" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</a>
-                        </li>
-                        <li>
-                        <a href="/" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Sign Out</a>
-                        </li>
-                    </ul>
+                        <ul className="font-medium text-lg flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                            <li>
+                                <Link to="/dashboard" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Dashboard</Link>
+                            </li>
+                            <li>
+                                <Link to="/statistic" className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Statistic</Link>
+                            </li>
+                            <li>
+                                <button onClick={handleLogout} className="block py-2 px-3 font-inter text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Sign Out</button>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
             <div className="mx-auto px-6 py-8 bg-gradient-to-b from-bright-blue to-soft-blue2">
-                <div className='bg-white rounded-lg'>
-                    <div className='flex flex-wrap items-center justify-between mx-auto'>
-                        <h3 className='text-lg font-inter font-semibold px-8 pt-4 md:px-8 md:py-4 md:text-xl'>{weekDays[day]}, {date}</h3>
-                        <h3 className='text-lg font-inter font-semibold px-8 py-2 md:px-8 md:py-4 md:text-xl'>{time}</h3>
+                <div className="bg-white rounded-lg">
+                    <div className="flex flex-wrap items-center justify-between mx-auto">
+                        <h3 className="text-lg font-inter font-semibold px-8 pt-4 md:px-8 md:py-4 md:text-xl">{weekDays[day]}, {date}</h3>
+                        <h3 className="text-lg font-inter font-semibold px-8 py-2 md:px-8 md:py-4 md:text-xl">{time}</h3>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-5 mb-4 px-7 pb-5">
                         <div className="bg-pure-blue p-6 rounded-lg shadow-md">
@@ -148,26 +105,12 @@ const Dashboard = () => {
                     <div className="w-full md:w-1/3 bg-white p-4 rounded-lg shadow-md">
                         <h3 className="text-xl font-inter font-semibold text-gray-700 mb-4">Task Lists</h3>
                         <ul>
-                            <li className="bg-pure-blue p-2 rounded mb-2">
-                                <p className="font-medium font-inter text-white">Task 1</p>
-                                <p className="text-white font-inter">08:00 - 10:00</p>
-                            </li>
-                            <li className="bg-pure-blue p-2 rounded mb-2">
-                                <p className="font-medium font-inter text-white">Task 2</p>
-                                <p className="text-white font-inter">Not Allocated</p>
-                            </li>
-                            <li className="bg-pure-blue p-2 rounded mb-2">
-                                <p className="font-medium font-inter text-white">Task 3</p>
-                                <p className="text-white font-inter">Not Allocated</p>
-                            </li>
-                            <li className="bg-pure-blue p-2 rounded mb-2">
-                                <p className="font-medium font-inter text-white">Task 4</p>
-                                <p className="text-white font-inter">Not Allocated</p>
-                            </li>
-                            <li className="bg-pure-blue p-2 rounded mb-2">
-                                <p className="font-medium font-inter text-white">Task 5</p>
-                                <p className="text-white font-inter">Not Allocated</p>
-                            </li>
+                            {tasks.map((task, index) => (
+                                <li key={index} className="bg-pure-blue p-2 rounded mb-2">
+                                    <p className="font-medium font-inter text-white">{task.name}</p>
+                                    <p className="text-white font-inter">{task.start} - {task.end}</p>
+                                </li>
+                            ))}
                         </ul>
                         <button 
                             className="w-full mt-4 py-2 bg-blue-500 text-white font-semibold font-inter rounded-lg opacity-90 hover:opacity-80"
@@ -179,46 +122,12 @@ const Dashboard = () => {
                     <div className="w-full md:w-2/3 bg-white p-4 rounded-lg shadow-md md:ml-4 mt-4 md:mt-0">
                         <h3 className="text-xl font-semibold font-inter text-gray-700 mb-4">Time Sheet</h3>
                         <div className="h-96 overflow-y-auto">
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 1</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 2</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 3</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 4</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 5</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 6</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 7</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 8</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 9</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
-                            <div className="bg-blue-500 text-white p-2 rounded mb-4">
-                                <p className="font-medium font-inter">Task 10</p>
-                                <p className='font-inter'>08:00 - 10:00</p>
-                            </div>
+                            {tasks.map((task, index) => (
+                                <div key={index} className="bg-blue-500 text-white p-2 rounded mb-4">
+                                    <p className="font-medium font-inter">{task.name}</p>
+                                    <p className='font-inter'>{task.start} - {task.end}</p>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
